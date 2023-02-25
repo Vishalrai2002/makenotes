d62ff2c83146083349bd0a00ca642221eed99e46
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-02-25 21:48:35 +0530

    video 45

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -1,12 +1,15 @@
 const express =require('express');
 const router =express.Router();
+const User =require('../models/User');
 
-router.get('/',(req,res)=>{
-    obj={
-        a:'thio',
-        number:23,
-    }
-    res.json(obj)
+// create a user using : POST "/api/auth"
+
+router.post('/',(req,res)=>{
+    
+    console.log(req.body);
+    const user =User(req.body);
+    user.save();
+    res.send(req.body);
 
 })
 
