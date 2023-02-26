9ebe5a2574bf934d5d103a2d96fd5100fbbfcb79
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-02-26 16:57:45 +0530

    video 50

--- backened/routes/auth.js
+++ backened/routes/auth.js
@@ -46,8 +46,40 @@ const secPass=await bcrypt.hash(req.body.password,salt);
 }
     catch(error){
         console.error(error.message);
-        res.status(500).send("some error occured");
+        res.status(500).send("Internal Server Error!");
     }
 })
 
+// create a user using : POST "/api/auth/login". NO login Required
+
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password can not be blank').exists(),
+],async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password}=req.body;
+    try {
+        let user =await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Please try to login with correct Credentials"});
+        }
+        const passwordCompare=await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please try to login with correct Credentials"});
+        }
+        const data={
+            user:{
+                id:user.id 
+            }
+        }
+        const authtoken =jwt.sign(data,JWT_SECRET);
+        res.json(authtoken);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error!");
+    }
+})
 module.exports=router
