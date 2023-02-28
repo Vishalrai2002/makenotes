e64b7096efff02f42c367a542755ab1b73ebf886
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-02-28 14:44:59 +0530

    video 56

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotes</h1>
-    </div>
+   <>
+   <Router>
+   <Navbar/>
+   <Routes>
+          <Route exact path="/" element={<Home/>}> </Route>
+          <Route exact path="/about" element={<About/>}> </Route>
+    </Routes>
+   </Router>
+   </>
   );
 }
 
