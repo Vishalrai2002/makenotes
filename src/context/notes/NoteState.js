d4119dec65f986b83079ce11f5ef62e5d2fcb4d7
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-03-02 16:34:15 +0530

    video 61

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -29,6 +29,33 @@ const NoteState=(props)=>{
           "tag": "personal",
           "date": "2023-02-27T09:18:40.748Z",
           "__v": 0
+        },
+        {
+          "_id": "63fc6ecb2a9841aa941435e3",
+          "user": "63fb3ab03872e1b87424a668",
+          "title": "My title",
+          "description": "Wake up early in the morning!",
+          "tag": "personal",
+          "date": "2023-02-27T08:50:19.073Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fc73a10900713dcdd86aaf",
+          "user": "63fb3ab03872e1b87424a668",
+          "title": "My title",
+          "description": "Wake up early in the morning!",
+          "tag": "personal",
+          "date": "2023-02-27T09:10:57.749Z",
+          "__v": 0
+        },
+        {
+          "_id": "63fc7570684936f3fd35d8a3",
+          "user": "63fb3ab03872e1b87424a668",
+          "title": "My title",
+          "description": "Wake up early in the morning!",
+          "tag": "personal",
+          "date": "2023-02-27T09:18:40.748Z",
+          "__v": 0
         }
       ]
 const [notes, setNotes] = useState(notesInitial)
