b0746062be705a3caa781f46e3a7113e6abea75a
Vishal <90440977+Vishalrai2002@users.noreply.github.com>
2023-03-03 07:52:44 +0530

    video 63

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -6,9 +6,9 @@ const NoteState=(props)=>{
         {
           "_id": "63fc6ecb2a9841aa91435e3",
           "user": "63fb3ab03872e1b87424a668",
-          "title": "My title",
-          "description": "Wake up early in the morning!",
-          "tag": "personal",
+          "title": "HI bro!",
+          "description": "Hi how are you!",
+          "tag": "tag",
           "date": "2023-02-27T08:50:19.073Z",
           "__v": 0
         },
@@ -59,8 +59,33 @@ const NoteState=(props)=>{
         }
       ]
 const [notes, setNotes] = useState(notesInitial)
+
+      // Add a Note
+      const addNote=(title,description,tag)=>{
+        const note={
+          "_id": "fc7570684936f3f5d8a878560",
+          "user": "63fb3ab03872e1b87424a668",
+          "title": title,
+          "description": description,
+          "tag":tag,
+          "date": "2023-02-27T09:18:40.748Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note))
+      }
+
+      // Delete a Note
+      const deleteNote=(id)=>{
+
+      }
+
+      // Edit a Note
+      const editNote=(id)=>{
+
+      }
+
     return (
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>
            {props.children}
         </NoteContext.Provider>
     )
